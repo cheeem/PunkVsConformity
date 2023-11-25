1af934a80ece2b12fb791ebec8fee376c1d19d2f
Jackson Werner <74575896+cheeem@users.noreply.github.com>
2023-11-25 16:34:29 -0600

    More Images, Content, Descriptions

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,42 @@
+let len: number = 0;
+let end: number = 0;
+
+const bar: HTMLDivElement = document.querySelector("#progress-bar")!;
+
 const observer: IntersectionObserver = new IntersectionObserver((entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {        
-	entries.forEach(entry => {
-		if(!entry.isIntersecting) return;
-		entry.target.classList.add("view");
-		observer.unobserve(entry.target);
+	console.log("--------")
+    entries.forEach(entry => {
+
+        const idx: number = parseInt(entry.target.className);
+
+		if(entry.isIntersecting) {
+            end = idx;
+        }
+
+        // const top: number = 0;
+        const height: number = (end + 1) / len * 100;
+
+        bar.setAttribute('style', `height: ${height}%;`);
+
+
 	});
 });
 
-const articles = document.querySelectorAll("article");
+const articles: NodeListOf<HTMLElement> = document.querySelectorAll("article");
 
 for(let i = 0; i < articles.length; i++) {
+
     const sections = articles[i].children;
+
+    len += sections.length;
+
     for(let j = 0; j < sections.length; j++) {
-        console.log(i, j);
+
+        const str = (len - sections.length + j).toString();
+
+        sections[j].className = str.toString();
+
+        observer.observe(sections[j]);
     }
+
 }
